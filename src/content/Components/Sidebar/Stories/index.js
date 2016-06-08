0984f28e4762efd7de644d992a0f5f7f8e7adc34
Nadim Tuhin <nadimtuhin@gmail.com>
2016-06-09 04:11:17 +0600

    working

--- src/content/Components/Sidebar/Stories/index.js
+++ src/content/Components/Sidebar/Stories/index.js
@@ -8,9 +8,11 @@ class Stories extends Component {
   render() {
     return (
       <div>
+        {!!store.story.allStories.length &&
         <div className="fba-search pure-form">
           <input placeholder="activity" onChange={e => store.story.search(e.target.value)} type="text" />
-        </div>
+        </div> }
+
 
         <div>
           {store.story.stories.map(story => <Story key={story.key} story={story} />)}
