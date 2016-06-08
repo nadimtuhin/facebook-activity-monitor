41965cca6bf98627c1df8c30e713cfb8fe0df67b
Nadim Tuhin <nadimtuhin@gmail.com>
2016-06-08 23:48:39 +0600

    getTIckerStories in fbTicker.js

--- src/content/utils/fbTicker.js
+++ src/content/utils/fbTicker.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+import map from 'lodash/map';
 import { select, scrollToBottom } from './dom';
 
 export function enableFbTicker () {
@@ -9,4 +11,22 @@ export function enableFbTicker () {
 export function fbTickerScroll () {
   const container = select('#pagelet_rhc_ticker .fbFeedTicker .uiScrollableArea .scrollable');
   scrollToBottom(container);
+}
+
+export function getTickerStories() {
+  let stories = document.querySelectorAll('.fbFeedTickerStory');
+  stories = map(stories, story => {
+    const $story = $(story);
+    const $content = $story.find('.tickerStoryBlock ._42ef');
+    const key = $story.attr('data-story-key');
+
+    return {
+      key,
+      link: $story.find('a.tickerStoryLink').attr('href'),
+      pp: $story.find('.lfloat._ohe img').attr('src'),
+      content: $content.html(),
+      text: $content.text()
+    }
+  });
+  return stories;
 }
